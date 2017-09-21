8c8038a74f3fb67d4382200b2febb858e4a37ea8
maitrey-shroff <maitrey89@me.com>
2017-09-20 23:51:02 -0700

    show all reviews for a movie page + more styling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,6 +14,16 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.create({movie_id: params[:movie_id], movie_name: params[:movie_name], email_address: params[:email_address], review_date: params[:review_date], rating: params[:rating], comment: params[:comment]})
+    redirect_to "/revues"
+  end
+
+  def show
+    @movie_id = params[:movie_id]
+
+    # @reviews = Review.where("movie_id ILIKE ?", "#{@movie_id}")
+    @reviews = Review.where(movie_id: @movie_id)
+
+    render :index
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/" => "reviews#index"
-  get "/movies" => "api/v1/movies#index"
+  get "/revues" => "reviews#index"
+  get "/" => "api/v1/movies#index"
 
   get "/new" => "reviews#new"
   post "/create" => "reviews#create"
+
+  get "/revues/:movie_id" => "reviews#show"
 
   namespace :api do
     namespace :v1 do
