e8679287b0d9b4afe7a403f616b74bad73045646
maitrey-shroff <maitrey89@me.com>
2017-09-22 00:35:42 -0700

    Hid API key

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::MoviesController < ApplicationController
     @movies = []
     x = 1
     while x<4 do
-      movies = Unirest.get("https://api.themoviedb.org/3/movie/now_playing?api_key=08680559d6fc65313f828456eb8b4b29&language=en-US&page=#{x}").body
+      movies = Unirest.get("https://api.themoviedb.org/3/movie/now_playing?api_key=#{ENV["API_KEY"]}&language=en-US&page=#{x}").body
       @movies = @movies + movies["results"]
       x = x+1
     end
