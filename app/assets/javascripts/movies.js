611e60a8eb33812533a806069c51b4e6351e2c24
maitrey-shroff <maitrey89@me.com>
2018-06-08 14:21:54 -0700

    implemented scalable sort by title and sort by date that uses API for page numbers

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -9,13 +9,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
     },
 
     mounted: function() {
+
+      this.searchMovies();
       // read URL params and pass them to below API call
 
       // Create a script that contains the data from the API request and call 'myFunc' function with that data as the argument. I'm using JSONP with a callback function to get the list of movies because The Movie Database does not allow standard AJAX calls
-      var s
-      s = document.createElement("script");
-      s.src = "https://api.themoviedb.org/3/discover/movie?api_key=08680559d6fc65313f828456eb8b4b29&language=en-US&sort_by=original_title.asc&include_adult=false&callback=app.myFunc";
-      document.body.appendChild(s);
+  //     var s
+  //     if (this.sortAttribute == null || this.sortAttribute == ''){
+  //       this.sortAttribute = 'original_title';
+  //     }
+  //     s = document.createElement("script");
+  //     s.src = "https://api.themoviedb.org/3/discover/movie?api_key=08680559d6fc65313f828456eb8b4b29&language=en-US&region=US&sort_by=" + this.sortAttribute + ".desc&include_adult=false&page=1&callback=app.myFunc";
+  //     document.body.appendChild(s);
 
 
       //AJAX call to get data from movies-app API (work around because The Movie Database API does not allow ajax requests)
@@ -31,15 +36,40 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     methods: {
 
+      searchMovies: function(){
+        var s;
+          if (this.sortAttribute == null || this.sortAttribute == ''){
+            this.sortAttribute = 'original_title';
+          }
+          if (this.pageNumber < 1 || this.pageNumber == null){
+            this.pageNumber = 1;
+          }
+          s = document.createElement("script");
+          s.src = "https://api.themoviedb.org/3/discover/movie?api_key=08680559d6fc65313f828456eb8b4b29&language=en-US&region=US&sort_by=" + this.sortAttribute + ".desc&include_adult=false&page=" + this.pageNumber + "&callback=app.myFunc";
+          document.body.appendChild(s);
+      },
+
       myFunc: function(result) {
       this.movies = result.results;      
       // console.log(result)
-    },
+      },
 
-      // setSortAttribute: function(inputSortAttribute) {
-      //   this.sortAttribute = inputSortAttribute;
-      //   this.sortAscending = !this.sortAscending;
-      // }
+      setSortAttribute: function(inputSortAttribute) {
+        this.sortAttribute = inputSortAttribute;
+        this.searchMovies();
+      },
+
+      nextPage: function() {
+        this.pageNumber++;
+        this.searchMovies();
+      },
+
+      prevPage: function() {
+        if (this.pageNumber >= 1){
+          this.pageNumber--;
+          this.searchMovies();
+        }
+      }
 
     },
 
