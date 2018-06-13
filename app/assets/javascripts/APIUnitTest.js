b8ce98357eb95d2f75352d8cd881be2ab36caccd
maitrey-shroff <maitrey89@me.com>
2018-06-13 12:04:26 -0700

    final delivery

--- app/assets/javascripts/APIUnitTest.js
+++
@@ -1,7 +0,0 @@
-import ('/../movies.js');
-
-describe("A suite", function() {
-  it("should go to the next", function() {
-    expect(methods.nextPage()).toEqual(2);
-  });
-});
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -2,19 +2,5 @@ require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
 
-  test 'valid review' do
-    review = Review.new(email_address: 'john@example.com', rating: '10')
-    assert review.valid?, 'review valid with email address and rating'
-  end
-
-  test 'invalid review without email address' do
-    review = Review.new(rating: '14')
-    assert review.invalid?, 'review invalid without email address'
-  end
-
-  test 'invalid without rating' do
-    review = Review.new(email_address: 'john@example.com')
-    assert review.invalid?, 'review invalid without rating'
-  end
 
 end
